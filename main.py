742d0e6295d93eb631a101dde9fb9d49f9c0bdb4
Ryan <59666279+Ry-Co@users.noreply.github.com>
2025-07-14 20:25:31 -0500

    pre-claude :D

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # hello world :D
-def main ():
-    print('hello world')
+
+import asyncio
+import api_handler
+from api_handler import chat
+
+
+def main():
+    user_input: str = input("Please input your query: ")
+    # TODO: parse user input for valid query
+    asyncio.run(chat(user_input))
+
 
 main()
-
-
-
-# 
